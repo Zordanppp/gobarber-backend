8e1c8d41627d47d56e9d7ddb6ddf907ab3a81111
Daniel Zordan <danielzordanfilho@gmail.com>
2020-09-29 17:51:11 -0400

    Finishing appointment routes & controllers

---
+++ src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+import ListProviderDayAvailabilityService from '@modules/appointments/services/ListProviderDayAvailabilityService';
+
+export default class ProvidersDayAvailabilityController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const { provider_id } = request.params;
+    const { month, day, year } = request.body;
+
+    const listProvidersDayAvailability = container.resolve(
+      ListProviderDayAvailabilityService,
+    );
+
+    const availability = await listProvidersDayAvailability.execute({
+      provider_id,
+      month,
+      day,
+      year,
+    });
+
+    return response.json(availability);
+  }
+}
--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -2,12 +2,24 @@ import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import ProvidersController from './../controllers/ProvidersController';
+import ProviderDayAvailabilityController from './../controllers/ProviderDayAvailabilityController';
+import ProviderMonthAvailabilityController from './../controllers/ProviderMonthAvailabilityController';
 
 const providesRouter = Router();
 const providersController = new ProvidersController();
+const providerDayAvailabilityController = new ProviderDayAvailabilityController();
+const providerMonthAvailabilityController = new ProviderMonthAvailabilityController();
 
 providesRouter.use(ensureAuthenticated);
 
 providesRouter.get('/', providersController.index);
+providesRouter.get(
+  '/providers/:provider_id/day-availability',
+  providerDayAvailabilityController.index,
+);
+providesRouter.get(
+  '/providers/:provider_id/month-availability',
+  providerMonthAvailabilityController.index,
+);
 
 export default providesRouter;
